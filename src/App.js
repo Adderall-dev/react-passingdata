7bd64b3f654ba3d2f3a323283908c3fb13413c10
Adderall-dev <urbaniak_edwin@wp.pl>
2020-09-16 20:41:24 +0200

    fix double function call

--- src/App.js
+++ src/App.js
@@ -15,10 +15,7 @@ export default function App() {
       {name.length < 3 ? <h1>input your name</h1> : <h1>hello, {name}!</h1>}
 
       <form onSubmit={handleSubmit}>
-        <InputComponent
-          onChange={(value) => setNameValue(value)}
-          onSubmit={handleSubmit}
-        />
+        <InputComponent onChange={(value) => setNameValue(value)} />
       </form>
     </div>
   );
